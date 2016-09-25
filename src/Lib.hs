ba297203985c7e5b39a4847aa6f8dcc607dd41ee
Paul Weaver <paul@ruthorn.co.uk>
2016-09-25 19:23:40 +0100

    Start trying to define stuff around Osc types and messages

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,5 +3,60 @@ module Lib
         someFunc,
     ) where
 
+import Path (Path, toOsc)
+import Data.Int (Int32, Int64)
+import qualified Data.Map.Strict as Map
+
 someFunc :: IO ()
 someFunc = putStrLn "Hello world"
+
+
+data OscValue = OscNil | OscBool Bool | OscTimeTag Int32 Int32 |
+    OscInt32 Int32 | OscInt64 Int64 | OscFloat Float | OscDouble Double |
+    OscChar Char | OscString String | OscSymbol String |
+    OscMidi {a :: Int32, b :: Int32, c :: Int32, d :: Int32} deriving (Eq, Show)
+
+oscTag :: OscValue -> Char
+oscTag (OscNil) = 'N'
+oscTag (OscBool x) = case x of
+    True -> 'T'
+    False -> 'F'
+oscTag (OscTimeTag _ _) = 't'
+oscTag (OscInt32 _) = 'i'
+oscTag (OscInt64 _) = 'h'
+oscTag (OscFloat _) = 'f'
+oscTag (OscDouble _) = 'd'
+oscTag (OscChar _) = 'c'
+oscTag (OscString _) = 's'
+oscTag (OscSymbol _) = 'S'
+oscTag (OscMidi _ _ _ _) = 'm'
+
+data OscMessage = OscMessage {oscMsgPath :: String, oscMsgArgs :: [OscValue]}
+
+data ClapiValue = Single OscValue | ClapiList [OscValue] deriving (Eq, Show)
+
+data ClapiMessage = ClapiMessage {
+    clapiMsgPath :: Path,
+    clapiMsgArgs :: [ClapiValue],
+    clapiMsgTags :: Map.Map String ClapiValue}
+
+toOscValue :: ClapiValue -> [OscValue]
+toOscValue (Single value) = [value]
+-- FIMXE: inefficient concatenation I think
+toOscValue (ClapiList values) = OscSymbol "[" : values ++ [OscSymbol "]"]
+
+toOscArgs :: [ClapiValue] -> [OscValue]
+toOscArgs = concatMap toOscValue
+
+-- FIXME: these names are bad - things which convert to OSC _from_ tags, etc...
+toOscTags :: Map.Map String ClapiValue -> [OscValue]
+toOscTags = Map.foldrWithKey myFunc [] where
+    myFunc tagName clapiValue accumulator =
+        (OscSymbol "?") : (OscString tagName) :
+        toOscValue clapiValue ++ accumulator
+
+toOscMessage :: ClapiMessage -> OscMessage
+toOscMessage cm =
+  OscMessage pathString argsList where
+      pathString = toOsc $ clapiMsgPath cm
+      argsList = (toOscArgs $ clapiMsgArgs cm) ++ (toOscTags $ clapiMsgTags cm)
