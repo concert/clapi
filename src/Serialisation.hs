b9c9252899141e461a56dc17d0e3dcf15db17063
Paul Weaver <paul@ruthorn.co.uk>
2016-10-02 14:02:44 +0100

    Rearrange Serialisation

--- src/Serialisation.hs
+++ src/Serialisation.hs
@@ -13,6 +13,16 @@ import Data.ByteString.Builder(floatBE, doubleBE)
 import Blaze.ByteString.Builder.ByteString (fromByteString)
 import Blaze.ByteString.Builder.Char.Utf8 (fromString, fromChar)
 
+prefixLength :: Builder -> Builder
+prefixLength b = byteSize bs <> fromByteString bs where
+    bs = toByteString b
+    {- FIXME: what do we do when the encoded string is more than 2^32 bytes
+    long? -}
+    byteSize = fromInt32be . fromIntegral . B.length
+
+encodeString :: String -> Builder
+encodeString = prefixLength . fromString
+
 data ClapiValue = CNil | CBool Bool | CTimeTag Int32 Int32 |
     CInt32 Int32 | CInt64 Int64 | CFloat Float | CDouble Double |
     CString String | CList [ClapiValue] deriving (Eq, Show)
@@ -41,13 +51,3 @@ addArg al cv = al <> encode' cv
 encodeList :: [ClapiValue] -> Builder
 encodeList vs = prefixLength (fst argList) <> snd argList where
     argList = foldl addArg mempty vs
-
-prefixLength :: Builder -> Builder
-prefixLength b = byteSize bs <> fromByteString bs where
-    bs = toByteString b
-    {- FIXME: what do we do when the encoded string is more than 2^32 bytes
-    long? -}
-    byteSize = fromInt32be . fromIntegral . B.length
-
-encodeString :: String -> Builder
-encodeString = prefixLength . fromString
