9886cb8776337516a9cf91332fd8ae9bec345803
Paul Weaver <paul@ruthorn.co.uk>
2016-10-14 20:38:32 +0100

    Remove redundent cruft from Lib

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,15 +4,6 @@ module Lib
         someFunc,
     ) where
 
-import Path (BasePath(..), Method(..))
-import Types (
-    ClapiMessage(..), ClapiPath, ClapiMethod(..), ClapiValue(..),
-    ClapiMessageTag, ClapiBundle
-    )
-import Blaze.ByteString.Builder (toByteString)
-import Data.Data (Typeable, Data, dataTypeOf, dataTypeConstrs, Constr, toConstr)
-import Data.Int (Int32, Int64)
-import qualified Data.Map.Strict as Map
 
 someFunc :: IO ()
 someFunc = putStrLn "Hello World"
