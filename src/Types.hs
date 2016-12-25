b6e000a757b3ee4742cc7ee3948aa9b8cdaacef7
Paul Weaver <paul@ruthorn.co.uk>
2016-12-25 00:48:04 +0000

    Avoid exceptions when converting ClapiValue -> native type

--- src/Types.hs
+++ src/Types.hs
@@ -31,51 +31,75 @@ data ClapiValue = CBool Bool | CTime Time |
     CWord32 Word32 | CWord64 Word64 |
     CInt32 Int32 | CInt64 Int64 |
     CFloat Float | CDouble Double |
-    CString T.Text | CList [ClapiValue] deriving (Eq, Show)
+    CString T.Text | CList [ClapiValue] deriving (Eq)
+
+instance Show ClapiValue where
+    show x = '_' : (show' x)
+      where
+        show' (CBool x) = show x
+        show' (CTime x) = show x
+        show' (CWord32 x) = show x
+        show' (CWord64 x) = show x
+        show' (CInt32 x) = show x
+        show' (CInt64 x) = show x
+        show' (CFloat x) = show x
+        show' (CDouble x) = show x
+        show' (CString x) = show x
+        show' (CList x) = show x
 
 class Clapiable a where
     toClapiValue :: a -> ClapiValue
-    fromClapiValue :: ClapiValue -> a
+    fromClapiValue :: ClapiValue -> Maybe a
 
 instance Clapiable Bool where
     toClapiValue = CBool
-    fromClapiValue (CBool x) = x
+    fromClapiValue (CBool x) = Just x
+    fromClapiValue _ = Nothing
 
 instance Clapiable Time where
     toClapiValue = CTime
-    fromClapiValue (CTime x) = x
+    fromClapiValue (CTime x) = Just x
+    fromClapiValue _ = Nothing
 
 instance Clapiable Word32 where
     toClapiValue = CWord32
-    fromClapiValue (CWord32 x) = x
+    fromClapiValue (CWord32 x) = Just x
+    fromClapiValue _ = Nothing
 
 instance Clapiable Word64 where
     toClapiValue = CWord64
-    fromClapiValue (CWord64 x) = x
+    fromClapiValue (CWord64 x) = Just x
+    fromClapiValue _ = Nothing
 
 instance Clapiable Int32 where
     toClapiValue = CInt32
-    fromClapiValue (CInt32 x) = x
+    fromClapiValue (CInt32 x) = Just x
+    fromClapiValue _ = Nothing
 
 instance Clapiable Int64 where
     toClapiValue = CInt64
-    fromClapiValue (CInt64 x) = x
+    fromClapiValue (CInt64 x) = Just x
+    fromClapiValue _ = Nothing
 
 instance Clapiable Float where
     toClapiValue = CFloat
-    fromClapiValue (CFloat x) = x
+    fromClapiValue (CFloat x) = Just x
+    fromClapiValue _ = Nothing
 
 instance Clapiable Double where
     toClapiValue = CDouble
-    fromClapiValue (CDouble x) = x
+    fromClapiValue (CDouble x) = Just x
+    fromClapiValue _ = Nothing
 
 instance Clapiable T.Text where
     toClapiValue = CString
-    fromClapiValue (CString x) = x
+    fromClapiValue (CString x) = Just x
+    fromClapiValue _ = Nothing
 
 instance Clapiable a => Clapiable [a] where
-    toClapiValue = CList . (map toClapiValue)
-    fromClapiValue (CList xs) = map fromClapiValue xs
+    toClapiValue = CList . (fmap toClapiValue)
+    fromClapiValue (CList xs) = sequence $ fmap fromClapiValue xs
+    fromClapiValue _ = Nothing
 
 data ClapiMethod = Error | Set | Add | Remove | Clear | Subscribe |
     Unsubscribe | AssignType | Children | Delete
