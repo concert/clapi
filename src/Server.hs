2af38ced2dbac4ec064198b5cfe9730bab23c9d5
Paul Weaver <paul@ruthorn.co.uk>
2017-01-24 22:49:51 +0000

    Make server fork to handle multiple requests
    Yes yes, it still just says "hello" at you...

--- src/Server.hs
+++ src/Server.hs
@@ -1,5 +1,7 @@
 module Server where
 
+import Control.Monad (forever)
+import Control.Concurrent (forkIO)
 import Network.Socket (
     Socket, PortNumber, Family(AF_INET), SocketType(Stream),
     SocketOption(ReuseAddr), SockAddr(SockAddrInet), socket, setSocketOption,
@@ -16,11 +18,16 @@ serve port =
     setSocketOption sock ReuseAddr 1  -- make socket immediately reusable - eases debugging.
     bind sock (SockAddrInet port iNADDR_ANY)
     listen sock 2  -- set a max of 2 queued connections  see maxListenQueue
-    action sock
+    handle sock action
+
+handle :: Socket -> (Socket -> IO ()) -> IO ()
+handle sock action = forever $
+  do
+    (sock', _) <- accept sock
+    forkIO (action sock')
 
 action :: Socket -> IO ()
 action sock =
-  do
-    (sock', _) <- accept sock
-    send sock' (toByteString . fromString $ "hello\n")
-    close sock'
+    do
+    send sock (toByteString . fromString $ "hello\n")
+    close sock
