741b1d35dfebe5121c10c9b415a26f05892e1911
Paul Weaver <paul@ruthorn.co.uk>
2017-01-26 00:32:55 +0000

    Add worker and corresponding channel to server
    So that our connecting threads can all queue work :)

--- src/Server.hs
+++ src/Server.hs
@@ -3,6 +3,8 @@ module Server where
 import Control.Monad (forever)
 import Control.Concurrent (forkIO)
 import Control.Exception (bracket)
+import Control.Concurrent.Chan.Unagi (
+    InChan, OutChan, newChan, dupChan, writeChan, readChan)
 import Network.Socket (
     Socket, PortNumber, Family(AF_INET), SocketType(Stream),
     SocketOption(ReuseAddr), SockAddr(SockAddrInet), socket, setSocketOption,
@@ -12,8 +14,15 @@ import Network.Socket.ByteString (send)
 import Blaze.ByteString.Builder (toByteString)
 import Blaze.ByteString.Builder.Char.Utf8 (fromString)
 
-serve :: (Socket -> IO ()) -> PortNumber -> IO ()
-serve action port = bracket startListening stopListening handleConnections
+type WriteChan a = InChan a
+type ReadChan a = OutChan a
+
+serve :: (Show a) => (Socket -> WriteChan a -> IO ()) -> PortNumber -> IO ()
+serve action port =
+  do
+    (inWrite, inRead) <- newChan
+    forkIO $ worker inRead  -- FIXME: how do we clean up the worker?
+    bracket startListening stopListening (handleConnections inWrite)
   where
     startListening = do
         sock <- socket AF_INET Stream 0
@@ -22,12 +31,16 @@ serve action port = bracket startListening stopListening handleConnections
         listen sock 2  -- set a max of 2 queued connections  see maxListenQueue
         return sock
     stopListening = close
-    handleConnections sock = forever $ do
+    handleConnections inWrite sock = forever $ do
         (sock', _) <- accept sock
-        forkIO (action sock')
+        forkIO (action sock' inWrite)
+    worker inRead = forever $ do
+        value <- readChan inRead
+        putStrLn $ show value
 
-action :: Socket -> IO ()
-action sock =
+action :: Socket -> WriteChan String -> IO ()
+action sock inWrite =
   do
     send sock (toByteString . fromString $ "hello\n")
+    writeChan inWrite "world"
     close sock
