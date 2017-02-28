b6244a9ae6e83e340474764f2b284ecc4fc3b869
Paul Weaver <paul@ruthorn.co.uk>
2017-02-28 18:56:29 +0000

    Example of two-stage killing of an IO from main thread

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
+import Control.Monad
+import Control.Concurrent
+import Control.Concurrent.Async
+import Control.Exception
+import System.Posix.Signals
+
 import Server
 
+
 main :: IO ()
-main = undefined
+main =
+  mask $ \restore -> do
+    a <- async $ mask_ (
+      (threadDelay 10000000 >> putStrLn "done")
+        `onException` (
+            (allowInterrupt >> putStrLn "poke" >> threadDelay 2000000 >> putStrLn "done poking")
+                `onException` putStrLn "poke2")
+        )
+    installHandler keyboardSignal (Catch $ cancel a) Nothing
+    restore $ wait a
