b6cdacadec12c141bb887adcb9fbab7589799f22
Paul Weaver <paul@ruthorn.co.uk>
2017-03-02 18:34:47 +0000

    Remove example code in main, use new withListen

--- app/Main.hs
+++ app/Main.hs
@@ -12,23 +12,10 @@ import Network.Simple.TCP hiding (send)
 
 import Server
 
--- thing :: ((Socket, SockAddr) -> IO r) -> IO r
-thing :: (Socket -> IO r) -> IO r
-thing serve = mask $ \restore -> do
-    (lsock, _) <- bindSock HostAny "1234"
-    NS.listen lsock 2048
-    addr <- NS.getSocketName lsock
-    -- a <- async $ serve (lsock, addr)
-    a <- async $ serve lsock
-    restore $ doubleCatch
-       (NS.close lsock >> wait a)
-       (cancel a >> putStrLn "success")
-       (wait a >>= \r -> NS.close lsock >> return r)
-
 main :: IO ()
 main =
   do
     tid <- myThreadId
     installHandler keyboardSignal (Catch $ killThread tid) Nothing
-    thing $
-        flip serve' $ \(hsock, _) -> send hsock "hello\n" >> threadDelay 10000000
+    withListen HostAny "1234" $ \(lsock, _) -> serve' lsock $
+        \(hsock, _) -> send hsock "hello\n" >> threadDelay 10000000
