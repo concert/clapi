230dbfe09843e407e867e8c81a6b362901b6d89c
Paul Weaver <paul@ruthorn.co.uk>
2017-04-09 00:28:23 +0100

    Add valuesToDef

--- src/Valuespace.hs
+++ src/Valuespace.hs
@@ -138,6 +138,36 @@ defToValues (ArrayDef l d t cl) =
   ]
 
 
+valuesToDef :: (MonadFail m) => MetaType -> [ClapiValue] -> m Definition
+valuesToDef
+    Tuple [l@(CEnum _), CString d, ns@(CList _), vds@(CList _), is@(CList _)] =
+  do
+    -- FIXME: need to be able to unpack enums
+    l' <- getEnum <$> fromClapiValue l
+    ns' <- fmap T.unpack <$> fromClapiValue ns
+    vds' <- fromClapiValue vds
+    is' <- Set.fromList <$> fmap getEnum <$> fromClapiValue is
+    tupleDef l' d ns' vds' is'
+valuesToDef Tuple _ = fail "bad types to define tuple"
+valuesToDef
+    Struct [l@(CEnum _), CString d, ns@(CList _), ts@(CList _), ls@(CList _)] =
+  do
+    l' <- getEnum <$> fromClapiValue l
+    ns' <- fmap T.unpack <$> fromClapiValue ns
+    ts' <- fmap T.unpack <$> fromClapiValue ts
+    ts'' <- mapM Path.fromString ts'
+    ls' <- fmap getEnum <$> fromClapiValue ls
+    structDef l' d ns' ts'' ls'
+valuesToDef Struct _ = fail "bad types to define struct"
+valuesToDef
+    Array [l@(CEnum _), CString d, CString t, cl@(CEnum _)] =
+  do
+    l' <- getEnum <$> fromClapiValue l
+    t' <- Path.fromString $ T.unpack t
+    cl' <- getEnum <$> fromClapiValue cl
+    arrayDef l' d t' cl'
+valuesToDef Array _ = fail "bad types to define array"
+
 
 data MetaType = Tuple | Struct | Array deriving (Eq, Show)
 type VsTree = ClapiTree [ClapiValue]
