016cb35ba816f8971f32893584aeab9d32c34674
Paul Weaver <paul@ruthorn.co.uk>
2017-04-08 13:45:22 +0100

    Add more general note function for Data.Maybe

--- src/Data/Maybe/Clapi.hs
+++ src/Data/Maybe/Clapi.hs
@@ -1,5 +1,8 @@
 module Data.Maybe.Clapi where
 
+import Prelude hiding (fail)
+import Control.Monad.Fail (MonadFail, fail)
+
 toMonoid :: (Monoid a) => Maybe a -> a
 toMonoid Nothing = mempty
 toMonoid (Just a) = a
@@ -8,3 +11,7 @@ fromFoldable :: (Foldable t) => t a -> Maybe (t a)
 fromFoldable t
   | null t = Nothing
   | otherwise = Just t
+
+note :: (MonadFail m) => String -> Maybe a -> m a
+note s Nothing = fail s
+note s (Just a) = return a
