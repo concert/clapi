eab660ce4ebd668f2277aa051aa3760983ca2bce
Paul Weaver <paul@ruthorn.co.uk>
2017-04-09 02:17:31 +0100

    Test Definition round trip

--- test/TestValuespace.hs
+++ test/TestValuespace.hs
@@ -1,21 +1,65 @@
+{-# LANGUAGE OverloadedStrings #-}
 module TestValuespace where
 
 import Test.HUnit (assertEqual)
 import Test.Framework.Providers.HUnit (testCase)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Test.QuickCheck (
+    Arbitrary(..), Gen, Property, arbitrary, oneof, elements, listOf, listOf1,
+    arbitraryBoundedEnum, vector, vectorOf)
 
+import Data.Either (either)
 import Data.Maybe (isNothing)
+import qualified Data.Text as T
 import Data.Word (Word16)
 import qualified Data.Map.Strict as Map
+import Control.Lens (view)
+import Control.Monad (replicateM)
 
-import Valuespace (getBaseValuespace, getTree)
+import Path (Path)
+import Types (InterpolationType)
+import Valuespace (
+    getBaseValuespace, getTree, Liberty, Definition(..), metaType, tupleDef,
+    structDef, arrayDef, defToValues, valuesToDef)
 import Tree (treeOrphansAndMissing)
 
 tests = [
-    testCase "test base valuespace" testBaseValuespace
+    testCase "test base valuespace" testBaseValuespace,
+    testProperty "Definition <-> ClapiValue round trip" propDefRoundTrip
     ]
 
 testBaseValuespace = assertEqual "clean base valuespace" (mempty, mempty) $
     treeOrphansAndMissing baseVsTree
     -- FIXME: this misses latent errors in the values because of laziness
   where
-    baseVsTree = getTree getBaseValuespace
+    baseVsTree = view getTree getBaseValuespace
+
+
+arbitraryPath :: Gen Path
+arbitraryPath = listOf $ listOf1 $ elements ['a'..'z']
+
+instance Arbitrary Liberty where
+    arbitrary = arbitraryBoundedEnum
+
+instance Arbitrary InterpolationType where
+    arbitrary = arbitraryBoundedEnum
+
+instance Arbitrary T.Text where
+    arbitrary = T.pack <$> arbitrary
+
+instance Arbitrary Definition where
+    arbitrary =
+      do
+        n <- arbitrary
+        fDef <- oneof [
+            tupleDef <$> arbitrary <*> arbitrary <*> vector n <*>
+                return (replicate n "bool") <*> arbitrary,
+            structDef <$> arbitrary <*> arbitrary <*> vector n <*>
+                vectorOf n arbitraryPath <*> vector n,
+            arrayDef <$> arbitrary <*> arbitrary <*> arbitraryPath <*>
+                arbitrary]
+        either error return fDef
+
+
+propDefRoundTrip :: Definition -> Bool
+propDefRoundTrip d = (valuesToDef (metaType d) . defToValues) d == Just d
