78dd9d8bd61c531b329f29bedbb578c1fa71d6bb
Paul Weaver <paul@ruthorn.co.uk>
2017-04-19 21:41:25 +0100

    Reinstate Definition round-trip testing
    with a slightly wonky hack to make unique valueNames.

--- test/TestValuespace.hs
+++ test/TestValuespace.hs
@@ -23,14 +23,14 @@ import Types (InterpolationType, ClapiValue(..))
 import Validator (validate)
 import Valuespace (
     --getBaseValuespace, getTree, Liberty, Definition(..), metaType, tupleDef,
-    Liberty(..), tupleDef,
-    structDef, arrayDef, definitionValidators, defToValues, valuesToDef)
+    Definition(..), Liberty(..), tupleDef, structDef, arrayDef, metaType,
+    definitionValidators, defToValues, valuesToDef)
 import Tree (treeOrphansAndMissing)
 
 tests = [
-    testCase "tupleDef" testTupleDef
+    testCase "tupleDef" testTupleDef,
     -- testCase "test base valuespace" testBaseValuespace,
-    -- testProperty "Definition <-> ClapiValue round trip" propDefRoundTrip
+    testProperty "Definition <-> ClapiValue round trip" propDefRoundTrip
     ]
 
 testTupleDef =
@@ -51,31 +51,36 @@ testTupleDef =
 --     baseVsTree = view getTree getBaseValuespace
 
 
--- arbitraryPath :: Gen Path
--- arbitraryPath = listOf $ listOf1 $ elements ['a'..'z']
+arbitraryPath :: Gen Path
+arbitraryPath = listOf $ listOf1 $ elements ['a'..'z']
 
--- instance Arbitrary Liberty where
---     arbitrary = arbitraryBoundedEnum
+instance Arbitrary Liberty where
+    arbitrary = arbitraryBoundedEnum
 
--- instance Arbitrary InterpolationType where
---     arbitrary = arbitraryBoundedEnum
+instance Arbitrary InterpolationType where
+    arbitrary = arbitraryBoundedEnum
 
--- instance Arbitrary T.Text where
---     arbitrary = T.pack <$> arbitrary
+instance Arbitrary T.Text where
+    arbitrary = T.pack <$> arbitrary
 
--- instance Arbitrary Definition where
---     arbitrary =
---       do
---         n <- arbitrary
---         fDef <- oneof [
---             tupleDef <$> arbitrary <*> arbitrary <*> vector n <*>
---                 return (replicate n "bool") <*> arbitrary,
---             structDef <$> arbitrary <*> arbitrary <*> vector n <*>
---                 vectorOf n arbitraryPath <*> vector n,
---             arrayDef <$> arbitrary <*> arbitrary <*> arbitraryPath <*>
---                 arbitrary]
---         either error return fDef
+-- http://stackoverflow.com/questions/9542313/
+infiniteStrings :: Int -> [String]
+infiniteStrings minLen = [minLen..] >>= (`replicateM` ['a'..'z'])
+
+instance Arbitrary Definition where
+    arbitrary =
+      do
+        n <- arbitrary
+        fDef <- oneof [
+            tupleDef <$> arbitrary <*> arbitrary <*>
+                (pure $ take n $ infiniteStrings 4) <*>
+                pure (replicate n "bool") <*> arbitrary,
+            structDef <$> arbitrary <*> arbitrary <*> vector n <*>
+                vectorOf n arbitraryPath <*> vector n,
+            arrayDef <$> arbitrary <*> arbitrary <*> arbitraryPath <*>
+                arbitrary]
+        either error return fDef
 
 
--- propDefRoundTrip :: Definition -> Bool
--- propDefRoundTrip d = (valuesToDef (metaType d) . defToValues) d == Just d
+propDefRoundTrip :: Definition -> Bool
+propDefRoundTrip d = (valuesToDef (metaType d) . defToValues) d == Just d
