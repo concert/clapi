319593b6cd809489fe47e75e34e00dff81c74efe
Paul Weaver <paul@ruthorn.co.uk>
2017-05-14 12:25:29 +0100

    Add Map.unionM
    Which doesn't just ignore the second Map's conents on unioning, but mappends!

--- src/Data/Map/Clapi.hs
+++ src/Data/Map/Clapi.hs
@@ -1,9 +1,13 @@
 module Data.Map.Clapi where
 
 import qualified Data.Map as Map
+import Data.Map.Strict.Merge (merge, preserveMissing, zipWithMatched)
 import Data.Monoid ((<>))
 
 import qualified Data.Maybe.Clapi as Maybe
+
+unionM :: (Monoid a, Ord k) => Map.Map k a -> Map.Map k a -> Map.Map k a
+unionM = merge preserveMissing preserveMissing (zipWithMatched (const (<>)))
 
 lookupM :: (Monoid a, Ord k) => k -> Map.Map k a -> a
 lookupM k m = Maybe.toMonoid $ Map.lookup k m
