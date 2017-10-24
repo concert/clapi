60618297faabd87ff8bb8c61b1a1fe20f8174676
David Honour <david@concertdaw.co.uk>
2017-10-24 19:01:46 +0100

    Add parsing of plain text representation

--- src/Clapi/TextSerialisation.hs
+++ src/Clapi/TextSerialisation.hs
@@ -1,8 +1,14 @@
 module Clapi.TextSerialisation where
 import Data.Monoid
+import Data.Text (Text)
+import Control.Applicative ((<|>))
 import qualified Data.Map as Map
+
 import Blaze.ByteString.Builder (Builder)
 import Blaze.ByteString.Builder.Char.Utf8 (fromString, fromChar)
+
+import Data.Attoparsec.Text (Parser, char, decimal, takeTill, many1, IResult(..), satisfy, inClass, parse, endOfInput)
+
 import qualified Clapi.Serialisation as Wire
 import Clapi.Types (Time(..), ClapiValue(..))
 
@@ -24,10 +30,49 @@ tpBuilder ((Time s f), vs) = tb <> mconcat (map (\v -> (fromChar ' ') <> (cvBuil
   where
     tb = (fss s) <> (fromChar ':') <> (fss f)
 
-encode :: Map.Map Time [ClapiValue] -> Builder
+type TimeSeries = Map.Map Time [ClapiValue]
+
+encode :: TimeSeries -> Builder
 encode ts = header <> bodyBuilder
   where
     -- It is invalid for a time series to be empty, so this use of head is
     -- kinda fine, but the errors will suck:
     header = fromString $ map typeTag $ head $ Map.elems ts
     bodyBuilder = mconcat $ map (\tp -> fromChar '\n' <> tpBuilder tp) (Map.toList ts)
+
+quotedString :: Parser Text
+-- FIXME: escaped quotes unsupported
+quotedString = char '"' >> takeTill (== '"')
+
+cvParser :: Char -> Parser ClapiValue
+cvParser 'B' = (wordMatch 'T' True) <|> (wordMatch 'F' False)
+  where
+    wordMatch c v = char c >> return (ClBool v)
+cvParser 'i' = decimal >>= return . ClInt32
+cvParser 's' = ClString <$> quotedString
+
+getTupleParser :: Parser (Parser [ClapiValue])
+getTupleParser = sequence <$> many1 (satisfy (inClass "Bis") >>= \c -> return (char ' ' >> cvParser c))
+
+timeParser :: Parser Time
+timeParser = do
+    s <- decimal
+    char ':'
+    f <- decimal
+    return $ Time s f
+
+decode :: Text -> Either String TimeSeries
+decode txt = case parse getTupleParser txt of
+    Fail _ ctxs msg -> Left msg
+    Partial _ -> Left "Cannot decode empty"
+    Done remaining p -> case parse (many1 $ innerParser p) remaining of
+        Fail _ _ msg -> Left msg
+        Partial _ -> Left "Unexpected EOI"
+        -- FIXME: nothing should be left over?
+        Done _ tsp -> Right $ Map.fromList tsp
+  where
+    innerParser tupleParser = do
+        char '\n'
+        t <- timeParser
+        vs <- tupleParser
+        return (t, vs)
--- test/TestTextSerialisation.hs
+++ test/TestTextSerialisation.hs
@@ -6,11 +6,17 @@ import Test.Framework.Providers.HUnit (testCase)
 import Blaze.ByteString.Builder (toByteString)
 
 import Clapi.Types (Time(..), ClapiValue(..))
-import Clapi.TextSerialisation (encode)
+import Clapi.TextSerialisation (encode, decode)
 
 tests = [
-   testCase "basic text encode" testBasicEncode
+   testCase "basic text encode" testBasicEncode,
+   testCase "basic text decode" testBasicDecode
    ]
 
-testBasicEncode = assertEqual "encode result" "Bis\n0:0 T 3 \"marimba\"" $
-    toByteString . encode $ Map.fromList [(Time 0 0, [ClBool True, ClInt32 3, ClString "marimba"])]
+timeseries = Map.fromList [(Time 0 0, [ClBool True, ClInt32 3, ClString "marimba"])]
+encoded _ = "Bis\n0:0 T 3 \"marimba\""
+
+testBasicEncode = assertEqual "encode result" (encoded undefined) $
+    toByteString . encode $ timeseries
+
+testBasicDecode = assertEqual "decode result" (Right timeseries) $ decode (encoded undefined)
