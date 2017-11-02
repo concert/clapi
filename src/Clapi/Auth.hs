bac08ab5cfc1eac72f3cf238f4f2ad1059ee309e
David Honour <david@concertdaw.co.uk>
2017-11-02 11:59:11 +0000

    Stub auth

---
+++ src/Clapi/Auth.hs
@@ -0,0 +1,22 @@
+module Clapi.Auth where
+import Control.Monad (forever)
+import qualified Data.ByteString as B
+
+import Clapi.Server (ClientEvent(..), ServerEvent(..), AddrWithUser(..))
+import Clapi.Protocol (Protocol, sendFwd, sendRev, waitThen)
+
+noAuth :: Monad m => Protocol
+    (ClientEvent i B.ByteString b)
+    (ClientEvent (AddrWithUser i Int) B.ByteString b)
+    (ServerEvent i B.ByteString)
+    -- FIXME: should be:
+    -- (ServerEvent i B.ByteString)
+    (ServerEvent (AddrWithUser i Int) B.ByteString)
+    m ()
+noAuth = forever $ waitThen fwd rev
+  where
+    fwd (ClientConnect i b) = sendFwd $ ClientConnect (AddrWithUser i "someone") b
+    fwd (ClientDisconnect i) = sendFwd $ ClientDisconnect (AddrWithUser i "someone")
+    fwd (ClientData i d) = sendFwd $ ClientData (AddrWithUser i "someone") d
+    rev (ServerData awu d) = sendRev $ ServerData (awuAddr awu) d
+    rev (ServerDisconnect awu) = sendRev $ ServerDisconnect (awuAddr awu)
