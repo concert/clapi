b3ee81511b701ca6cffca96ac3f671e2264eafd4
David Honour <david@concertdaw.co.uk>
2017-11-06 15:14:41 +0000

    Have a go with toClapiValue...
    turns out not so useful due to large number of strings that need
    coercing

--- test/TestRelay.hs
+++ test/TestRelay.hs
@@ -6,7 +6,8 @@ import qualified Data.Text as T
 import Test.Framework.Providers.HUnit (testCase)
 import Test.HUnit (assertEqual)
 
-import Clapi.Types (Message(..), Interpolation(IConstant), Time(..), ClapiValue(..))
+import Path.Parsing (toString)
+import Clapi.Types (Message(..), Interpolation(IConstant), Time(..), ClapiValue(..), Enumerated(..), toClapiValue)
 import Clapi.Valuespace (getType, baseValuespace, Liberty(..))
 import Clapi.Relay (handleMessages)
 import Clapi.NamespaceTracker (RoutableMessage(..), Ownership(..))
@@ -37,12 +38,11 @@ testRootStructExtend = assertMsgsInclude expectedMsgs rMsgs
     expectedMsgs = inMsgs ++ [MsgSet (tp []) z extendedV IConstant Nothing Nothing]
     z = Time 0 0
     extendedV = [cannot, s "auto-generated container", los ["foo", "api"], lop [["foo", "type"], tp ["api"]], ClList [cannot, cannot]]
-    s = ClString
-    los ss = ClList $ map ClString ss
-    -- FIXME: Writing this path stringy thing all over the place
-    lop ps = los $ map (\p -> T.pack $ "/" ++ (concat $ intersperse "/" p)) ps
+    s = ClString . T.pack
+    los = ClList . (map s)
+    lop ps = los $ map toString ps
     tp p = fromJust $ getType baseValuespace p
     om = map (\m -> (Owner, m))
     (ms', vs') = handleMessages baseValuespace 0 $ om inMsgs
     rMsgs = map rmMsg ms'
-    cannot = ClEnum $ fromIntegral $ fromEnum Cannot
+    cannot = toClapiValue $ Enumerated Cannot
