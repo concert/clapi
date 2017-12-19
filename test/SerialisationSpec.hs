ed954e39b7bfa3975fb0b8c7248d00d38d5a1f33
Paul Weaver <paul@concertdaw.co.uk>
2017-12-19 14:21:01 +0000

    Add even more arbitrary bundleage to serialisation tests

--- test/SerialisationSpec.hs
+++ test/SerialisationSpec.hs
@@ -5,7 +5,7 @@ module SerialisationSpec where
 
 import Test.Hspec
 import Test.QuickCheck
-  (property, counterexample, Arbitrary(..), Gen, oneof)
+  (property, Property, counterexample, Arbitrary(..), Gen, oneof)
 import Test.QuickCheck.Instances ()
 
 import Data.ByteString (ByteString)
@@ -16,8 +16,11 @@ import Data.Word (Word16)
 import Clapi.Path (Path(..), Name)
 import Clapi.Types
   ( CanFail, Time, Attributee, Site, ClapiValue, RequestBundle
-  , Interpolation(..), SubMessage(..), RequestBundle(..), DataUpdateMessage(..))
-import Clapi.Serialisation (encode, decode)
+  , Interpolation(..), SubMessage(..), DataUpdateMessage(..)
+  , TreeUpdateMessage(..), RequestBundle(..)  , UpdateBundle(..)
+  , OwnerRequestBundle(..), FromRelayBundle(..)  , ToRelayBundle(..)
+  , UMsgError(..))
+import Clapi.Serialisation (Serialisable, encode, decode)
 
 import TypesSpec (smallListOf, name) -- Incl. Arbitrary instances of ClapiValue
 
@@ -32,11 +35,6 @@ instance Arbitrary SubMessage where
   arbitrary = oneof
       [ UMsgSubscribe <$> arbitrary
       , UMsgUnsubscribe <$> arbitrary]
-
-instance Arbitrary RequestBundle where
-  arbitrary = RequestBundle <$> arbitrary <*> arbitrary
-  shrink (RequestBundle subs msgs) =
-    [RequestBundle subs' msgs' | (subs', msgs') <- shrink (subs, msgs)]
 
 instance Arbitrary DataUpdateMessage where
   arbitrary = oneof
@@ -84,27 +82,65 @@ instance Arbitrary DataUpdateMessage where
   shrink (UMsgSetChildren p ns a) =
     [UMsgSetChildren p' ns' a' | (p', ns', a') <- shrink (p, ns, a)]
 
+instance Arbitrary TreeUpdateMessage where
+  arbitrary = oneof
+    [ UMsgAssignType
+      <$> (arbitrary :: Gen Path)
+      <*> (arbitrary :: Gen Path)
+    , UMsgDelete <$> (arbitrary :: Gen Path)]
+
+instance Arbitrary UMsgError where
+  arbitrary = UMsgError <$> arbitrary <*> arbitrary
+
+instance Arbitrary RequestBundle where
+  arbitrary = RequestBundle <$> smallListOf arbitrary <*> smallListOf arbitrary
+  shrink (RequestBundle subs msgs) =
+    [RequestBundle subs' msgs' | (subs', msgs') <- shrink (subs, msgs)]
+
+instance Arbitrary UpdateBundle where
+  arbitrary = UpdateBundle <$> smallListOf arbitrary <*> smallListOf arbitrary
+
+instance Arbitrary OwnerRequestBundle where
+  arbitrary =
+    OwnerRequestBundle <$> smallListOf arbitrary <*> smallListOf arbitrary
+
+instance Arbitrary FromRelayBundle where
+  arbitrary = oneof [FRBClient <$> arbitrary, FRBOwner <$> arbitrary]
+
+instance Arbitrary ToRelayBundle where
+  arbitrary = oneof [TRBClient <$> arbitrary, TRBOwner <$> arbitrary]
+
 
 showBytes :: ByteString -> String
 showBytes = show . BS.unpack
 
 
-spec :: Spec
-spec =
-    describe "serialisation" $ do
-      it "should fail to encode overly long string" $
-        let
-          n = fromIntegral (maxBound :: Word16)
-          longStr = replicate (n + 1) 'a'
-        in
-          encode longStr `shouldSatisfy` isLeft
+propBundleRoundTrip
+  :: forall bundle. (Show bundle, Eq bundle, Serialisable bundle)
+  => bundle -> Property
+propBundleRoundTrip b = let
+    mbs = encode b
+    result = mbs >>= decode :: CanFail bundle
+  in
+    counterexample (show mbs) $
+    counterexample (show $ showBytes <$> mbs) $
+    counterexample (show result) $
+    Right b == result
 
-      it "should survive a round trip via binary" $ property $
-        \(b :: RequestBundle) -> let
-            mbs = encode b
-            result = mbs >>= decode :: CanFail RequestBundle
-          in
-            counterexample (show mbs) $
-            counterexample (show $ showBytes <$> mbs) $
-            counterexample (show result) $
-            Right b == result
+
+spec :: Spec
+spec = do
+  it "should fail to encode overly long string" $
+    let
+      n = fromIntegral (maxBound :: Word16)
+      longStr = replicate (n + 1) 'a'
+    in
+      encode longStr `shouldSatisfy` isLeft
+
+  describe "ToRelayBundle" $
+    it "should survive a round trip via binary" $ property $
+      \(b :: ToRelayBundle) -> propBundleRoundTrip b
+
+  describe "FromRelayBundle" $
+    it "should survive a round trip via binary" $ property $
+      \(b :: FromRelayBundle) -> propBundleRoundTrip b
