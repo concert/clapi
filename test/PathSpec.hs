a9c3cd822693fd9f1803fdcdf74dd8b7a9db9bd8
David Honour <david@concertdaw.co.uk>
2017-12-19 08:31:48 +0000

    Rework path spec to be hspeccy

--- test/PathSpec.hs
+++ test/PathSpec.hs
@@ -3,54 +3,33 @@ module PathSpec where
 
 import Test.Hspec
 import Test.QuickCheck (property)
-import Test.HUnit ((@=?), assertEqual)
-import Test.Framework.Providers.HUnit (testCase)
-import Test.Framework.Providers.QuickCheck2 (testProperty)
 
-import Data.Maybe (isNothing)
-import Data.Word (Word16)
-import qualified Data.Map.Strict as Map
+import Data.Either (isLeft)
 
-import Helpers (assertFailed)
-import Clapi.Path (Path(..), root, up)
+import Clapi.Path (Path(..), Name, root, up)
 import Clapi.PathQ (pathq)
 import Path.Parsing (toString, fromString)
-import Clapi.Serialisation (encode, decode)
+import Clapi.Types (CanFail)
 
-
-tests = [
-    testCase "roundtrip path" testPathRoundtrip,
-    testCase "path fromString" testPathFromString,
-    testCase "up" testUp
-    -- testCase "pathQ" testPathQ
-    ]
-
-testPathRoundtrip =
-  do
-    rt "empty path" root
-    rt "top-level path" $ Path ["foo"]
-    rt "nested path" $ Path ["foo_1", "bar_2"]
-  where
-    rt s path = assertEqual s (fromString $ toString path) (Just path)
-
-testPathFromString =
-  do
-    assertFailed "empty" (fromString "")
-    fromString "/" >>= assertEqual "slash" root
-    fromString "/foo" >>= assertEqual "single" (Path ["foo"])
-    fromString "/foo/bar" >>= assertEqual "multiple" (Path ["foo", "bar"])
-    -- FIXME: should the following strictly be an error?
-    -- assertFailed "trailing" (fromString "/foo/bar/")
-    assertFailed "no leading" (fromString "foo/bar")
-
-testUp =
-  do
-    assertEqual "root up failed" root $ up root
-    assertEqual "normal up failed" (Path ["a"]) $ up $ Path ["a", "b"]
 
 spec :: Spec
 spec = do
     describe "Quasiquoter" $ it "Produces expected path" $ [pathq|/oi/mate|] `shouldBe` Path ["oi", "mate"]
-    describe "Failything" $ it "Fails" $ 1 `shouldBe` 2
-    describe "Props" $ it "isEven" $ property (\x -> even $ (x :: Int) * 2)
-    describe "Faility" $ it "isDubiouslyEven" $ property (\x -> even $ (x :: Int))
+    describe "Up" $ do
+        it "Gets the parent" $ up (Path ["a", "b"]) `shouldBe` Path ["a"]
+        it "Does nothing at the root" $ up root `shouldBe` root
+    describe "From string" $ do
+        it "Root" $ "/" `shouldBeGoodPath` []
+        it "Single segment" $ "/foo" `shouldBeGoodPath` ["foo"]
+        it "Multi segment" $ "/foo/bar" `shouldBeGoodPath` ["foo", "bar"]
+        it "Trailing /" $ "/foo/bar/" `shouldBeGoodPath` ["foo", "bar"]
+        it "Fails with no leading /" $ shouldBeBadPath "foo/bar"
+        it "No leading /" $ shouldBeBadPath "foo/bar"
+    describe "Round trip" $ do
+        it "Empty path" $ rt root
+        it "Top level path" $ rt $ Path ["fdoo"]
+        it "Nested path" $ rt $ Path ["boo_1", "chew_2"]
+  where
+    shouldBeGoodPath str segs = (fromString str :: CanFail Path) `shouldBe` (Right $ Path segs)
+    shouldBeBadPath str = (fromString str :: CanFail Path) `shouldSatisfy` isLeft
+    rt p = (fromString $ toString p :: CanFail Path) `shouldBe` Right p
