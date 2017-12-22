053ff00196515837041d1ee1d6e33275cd869447
Paul Weaver <paul@concertdaw.co.uk>
2017-12-22 10:40:21 +0000

    Wall Clapi.TimeDelta

--- src/Clapi/TimeDelta.hs
+++ src/Clapi/TimeDelta.hs
@@ -1,15 +1,20 @@
+{-# OPTIONS_GHC -Wall -Wno-orphans #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-module Clapi.TimeDelta (TimeDelta, getDelta, tdZero) where
-import Data.Word (Word32)
-import System.Clock (Clock(Monotonic), TimeSpec, getTime, toNanoSecs)
 
-import Clapi.Types (Time(..), TimeStamped(..), ClapiValue(ClFloat), Clapiable(..))
+module Clapi.TimeDelta where -- (TimeDelta, getDelta, tdZero) where
+
+import Data.Word (Word32)
+import System.Clock (Clock(Monotonic), getTime, toNanoSecs)
+
+import Clapi.Types (Time(..), ClapiValue(ClFloat), Clapiable(..))
 
 timeToFloat :: Time -> Float
-timeToFloat (Time s f) = fromRational $ toRational s + (toRational f / toRational (maxBound :: Word32))
+timeToFloat (Time s f) =
+  fromRational $ toRational s + (toRational f / toRational (maxBound :: Word32))
 
 getMonotonicTimeFloat :: IO Float
-getMonotonicTimeFloat = getTime Monotonic >>= return . fromRational . (/10.0^9) . toRational . toNanoSecs
+getMonotonicTimeFloat = getTime Monotonic >>=
+    return . fromRational . (/10.0^(9 :: Int)) . toRational . toNanoSecs
 
 newtype TimeDelta = TimeDelta Float deriving Num
 
@@ -17,7 +22,8 @@ tdZero :: TimeDelta
 tdZero = TimeDelta 0.0
 
 getDelta :: Time -> IO TimeDelta
-getDelta theirTime = getMonotonicTimeFloat >>= return . TimeDelta . subtract (timeToFloat theirTime)
+getDelta theirTime = getMonotonicTimeFloat >>=
+  return . TimeDelta . subtract (timeToFloat theirTime)
 
 instance Clapiable TimeDelta where
     toClapiValue (TimeDelta f) = ClFloat f
