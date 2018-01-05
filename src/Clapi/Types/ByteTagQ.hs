00c6ec182d51cda173f5549ad6effce84b12e7a6
Paul Weaver <paul@concertdaw.co.uk>
2018-01-05 19:22:38 +0000

    Add ByteTagQ
    For making representable Word8s at compile time.

---
+++ src/Clapi/Types/ByteTagQ.hs
@@ -0,0 +1,35 @@
+{-# OPTIONS_GHC -Wall -Wno-orphans #-}
+{-# LANGUAGE
+    QuasiQuotes
+  , TemplateHaskell
+  , ScopedTypeVariables
+#-}
+module Clapi.Types.ByteTagQ (btq) where
+
+import Control.Monad ((>=>))
+import Control.Monad.Fail (MonadFail)
+import qualified Control.Monad.Fail as MF
+import Data.Word
+import Language.Haskell.TH.Lift (lift)
+import Language.Haskell.TH.Quote (QuasiQuoter(..))
+
+btq :: QuasiQuoter
+btq = QuasiQuoter {
+    quoteExp = fromStr >=> lift,
+    quotePat = fail "Not supported",
+    quoteDec = fail "Not supported",
+    quoteType = fail "Not supported"}
+  where
+    fromStr [c] = fromChar c
+    fromStr _ = fail "Not one char"
+    fromChar :: MonadFail m => Char -> m Word8
+    fromChar = bound
+    bound :: forall a b m. (Enum a, Enum b, Bounded b, MonadFail m) => a -> m b
+    bound i = let
+        low = fromEnum (minBound :: b)
+        high = fromEnum (maxBound :: b)
+        v = fromEnum i
+      in
+        if low <= v && v <= high
+          then return $ toEnum v
+          else MF.fail "out of bounds"
