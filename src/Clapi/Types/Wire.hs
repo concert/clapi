1c7e68c678fa31f6cc760d66abb7f6b9d9ff1985
Paul Weaver <paul@concertdaw.co.uk>
2018-01-06 00:15:18 +0000

    Simplify Wire. A lot.

--- src/Clapi/Types/Wire.hs
+++ src/Clapi/Types/Wire.hs
@@ -1,146 +1,58 @@
 {-# OPTIONS_GHC -Wall -Wno-orphans #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module Clapi.Types.Wire
-  ( WireConcreteType(..), WireContainerType(..), WireType(..)
-  , Wireable, toHaskell
-  , WireValue, wireValue, wireTypeOf
+  ( Wireable -- , toHaskell
+  , WireValue(..)
+  , apply
   ) where
 
 import Prelude hiding (fail)
 import Control.Monad.Fail (MonadFail(..))
 import Data.Int
-import Data.Proxy
 import Data.Text (Text)
 import Data.Word
-import Unsafe.Coerce
+import Data.Typeable
 
 import Clapi.Types.Base (Time)
 
-data WireConcreteType
-  = WcTime
-  | WcWord8 | WcWord32 | WcWord64
-  | WcInt32 | WcInt64
-  | WcFloat | WcDouble
-  | WcString
-  deriving (Show, Eq, Ord, Enum, Bounded)
+cast' :: forall a b m. (Typeable a, Typeable b, MonadFail m) => a -> m b
+cast' a =
+  let
+    die = fail $ "Type mismatch: tried to cast value of type "
+      ++ show (typeOf a) ++ " to " ++ show (typeRep (Proxy :: Proxy b))
+  in
+    maybe die return $ cast a
 
-data WireContainerType
-  = WcList
-  deriving (Show, Eq, Ord, Enum, Bounded)
-
-data WireType
-  = WtConc WireConcreteType
-  | WtCont WireContainerType WireType
-  deriving (Show, Eq, Ord)
-
-getTyInfo :: WireType -> ([WireContainerType], WireConcreteType)
-getTyInfo wt = let (l, t) = inner wt in (reverse l, t)
-  where
-    inner (WtConc concT) = ([], concT)
-    inner (WtCont contT wt') =
-        let (l, concT) = getTyInfo wt' in (contT : l, concT)
-
-class (Show a, Eq a, Ord a) => Wireable a where
-  toWireType :: proxy a -> WireType
-  toHaskell :: MonadFail m => WireValue -> m a
-  toHaskell (WireValue wt v) =
-    let expectedType = toWireType (Proxy :: Proxy a) in
-    if wt == expectedType
-    then return $ unsafeCoerce v
-    else fail $ "Type mismatch: tried to unpack value of "
-         ++ show wt ++ " as " ++ show expectedType
-
+class (Typeable a, Show a, Eq a, Ord a) => Wireable a
 instance Wireable Time where
-  toWireType _ = WtConc WcTime
+instance Wireable Word8
+instance Wireable Word32
+instance Wireable Word64
+instance Wireable Int32
+instance Wireable Int64
+instance Wireable Float
+instance Wireable Double
+instance Wireable Text
+instance Wireable a => Wireable [a]
 
-instance Wireable Word8 where
-  toWireType _ = WtConc WcWord8
-instance Wireable Word32 where
-  toWireType _ = WtConc WcWord32
-instance Wireable Word64 where
-  toWireType _ = WtConc WcWord64
+data WireValue = forall a. Wireable a => WireValue a
 
-instance Wireable Int32 where
-  toWireType _ = WtConc WcInt32
-instance Wireable Int64 where
-  toWireType _ = WtConc WcInt64
-
-instance Wireable Float where
-  toWireType _ = WtConc WcFloat
-instance Wireable Double where
-  toWireType _ = WtConc WcDouble
-
-instance Wireable Text where
-  toWireType _ = WtConc WcString
-
-instance Wireable a => Wireable [a] where
-  toWireType _ = WtCont WcList $ toWireType (Proxy :: Proxy a)
-
-
-data WireValue = forall a. Wireable a => WireValue WireType a
-
-wireValue :: forall a. Wireable a => a -> WireValue
-wireValue a = WireValue (toWireType (Proxy :: Proxy a)) a
-
-wireTypeOf :: WireValue -> WireType
-wireTypeOf (WireValue wt _) = wt
-
-
-instance Show WireValue where
-  show = wireApply show
+deriving instance Show WireValue
 
 instance Eq WireValue where
-  (==) = binaryWireApply (==) (==)
+  (WireValue x) == (WireValue y) = maybe False (x ==) $ cast y
 
 instance Ord WireValue where
-  compare = binaryWireApply compare compare
+  compare (WireValue x) (WireValue y) =
+    maybe (compare (typeOf x) (typeOf y)) (compare x) $ cast y
 
+castWireValue :: (MonadFail m, Wireable a) => WireValue -> m a
+castWireValue (WireValue x) = cast' x
 
-wireApply :: forall r. (forall x. Wireable x => x -> r) -> WireValue -> r
-wireApply f (WireValue wt v) =
-  let (tyL, concT) = getTyInfo wt in case concT of
-    WcTime -> nestWire (Proxy :: Proxy Time) tyL
-    WcWord8 -> nestWire (Proxy :: Proxy Word8) tyL
-    WcWord32 -> nestWire (Proxy :: Proxy Word32) tyL
-    WcWord64 -> nestWire (Proxy :: Proxy Word64) tyL
-    WcInt32 -> nestWire (Proxy :: Proxy Int32) tyL
-    WcInt64 -> nestWire (Proxy :: Proxy Int64) tyL
-    WcFloat -> nestWire (Proxy :: Proxy Float) tyL
-    WcDouble -> nestWire (Proxy :: Proxy Double) tyL
-    WcString -> nestWire (Proxy :: Proxy Text) tyL
-  where
-    nestWire
-      :: forall a. Wireable a
-      => Proxy a -> [WireContainerType] -> r
-    nestWire _ [] = f @a $ unsafeCoerce v
-    nestWire _ (contT:contTs) = case contT of
-      WcList -> nestWire (Proxy :: Proxy [a]) contTs
-
-binaryWireApply
-  :: forall r.
-     (WireType -> WireType -> r)
-  -> (forall x. Wireable x => x -> x -> r)
-  -> WireValue -> WireValue -> r
-binaryWireApply onTyMismatch f (WireValue wt0 v0) (WireValue wt1 v1)
-  | wt0 == wt1 = let (tyL, concT) = getTyInfo wt0 in case concT of
-      WcTime -> nestWire (Proxy :: Proxy Time) tyL
-      WcWord8 -> nestWire (Proxy :: Proxy Word8) tyL
-      WcWord32 -> nestWire (Proxy :: Proxy Word32) tyL
-      WcWord64 -> nestWire (Proxy :: Proxy Word64) tyL
-      WcInt32 -> nestWire (Proxy :: Proxy Int32) tyL
-      WcInt64 -> nestWire (Proxy :: Proxy Int64) tyL
-      WcFloat -> nestWire (Proxy :: Proxy Float) tyL
-      WcDouble -> nestWire (Proxy :: Proxy Double) tyL
-      WcString -> nestWire (Proxy :: Proxy Text) tyL
-  | otherwise = onTyMismatch wt0 wt1
-  where
-    nestWire
-      :: forall a. Wireable a
-      => Proxy a -> [WireContainerType] -> r
-    nestWire _ [] = f @a (unsafeCoerce v0) (unsafeCoerce v1)
-    nestWire _ (contT:contTs) = case contT of
-      WcList -> nestWire (Proxy :: Proxy [a]) contTs
+apply
+  :: (Wireable a, Wireable b, MonadFail m)
+  => (a -> b) -> WireValue -> m WireValue
+apply f = fmap (WireValue . f) . castWireValue
