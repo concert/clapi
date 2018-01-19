397b1a12dced5bf4591fd0a8817760da2a00ef91
Paul Weaver <paul@concertdaw.co.uk>
2018-01-19 17:21:02 +0000

    Add some helpers for AssocList

--- src/Clapi/Types/AssocList.hs
+++ src/Clapi/Types/AssocList.hs
@@ -1,21 +1,35 @@
 {-# OPTIONS_GHC -Wall -Wno-orphans #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE TupleSections #-}
 
 module Clapi.Types.AssocList
-  ( mkAssocList, unAssocList, AssocList, alFromMap, alFromZip, alLookup, alKeys
-  , alValues ) where
+  ( mkAssocList, unAssocList, AssocList, alSingleton, alFromKeys, alFromMap
+  , alFromZip, alLookup, alKeys, alValues, alFmapWithKey, alAlterF, alAlter
+  , alAdjust
+  ) where
 
 import Prelude hiding (fail)
 import Control.Monad.Fail (MonadFail(..))
+import qualified Data.Foldable as Foldable
+import Data.Functor.Identity (Identity(..))
 import Data.Map (Map)
 import qualified Data.Map as Map
 
+import Clapi.Types.UniqList (UniqList, unUniqList)
 import Clapi.Util (ensureUnique, strictZip, fmtStrictZipError)
 
-newtype AssocList a b = AssocList {unAssocList :: [(a, b)]} deriving (Show, Eq, Functor)
+newtype AssocList a b
+  = AssocList {unAssocList :: [(a, b)]} deriving (Show, Eq, Functor, Foldable)
 
 mkAssocList :: (MonadFail m, Ord a, Show a) => [(a, b)] -> m (AssocList a b)
 mkAssocList abPairs = ensureUnique "keys" (fmap fst abPairs) >> return (AssocList abPairs)
+
+alSingleton :: a -> b -> AssocList a b
+alSingleton a b = AssocList [(a, b)]
+
+alFromKeys :: b -> UniqList a -> AssocList a b
+alFromKeys b as = AssocList $ zip (unUniqList as) $ repeat b
 
 alFromMap :: Map a b -> AssocList a b
 alFromMap = AssocList . Map.toList
@@ -31,3 +45,22 @@ alKeys = fmap fst . unAssocList
 
 alValues :: AssocList a b -> [b]
 alValues = fmap snd . unAssocList
+
+alFmapWithKey :: (k -> b -> c) -> AssocList k b -> AssocList k c
+alFmapWithKey f (AssocList kbs) = AssocList $ (\(k, b) -> (k, f k b)) <$> kbs
+
+alAlterF
+  :: (Eq k, Functor f) => (Maybe b -> f (Maybe b)) -> k -> AssocList k b
+  -> f (AssocList k b)
+alAlterF f k (AssocList theKbs) = AssocList <$> alterF theKbs
+  where
+    alterF [] = Foldable.toList . fmap (k,) <$> f Nothing
+    alterF (p@(k', b):kbs)
+      | k' == k = maybe kbs (\b' -> (k, b') : kbs) <$> f (Just b)
+      | otherwise = (p:) <$> alterF kbs
+
+alAlter :: Eq k => (Maybe b -> Maybe b) -> k -> AssocList k b -> AssocList k b
+alAlter f k = runIdentity . alAlterF (Identity . f) k
+
+alAdjust :: Eq k => (b -> b) -> k -> AssocList k b -> AssocList k b
+alAdjust f k = alAlter (fmap f) k
