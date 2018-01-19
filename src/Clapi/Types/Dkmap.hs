b2a0e50c1d980fb795d765126869602396e0898b
Paul Weaver <paul@concertdaw.co.uk>
2018-01-19 17:20:18 +0000

    Define a double-keyed map to help us with a new formulation of time series
    where clients refer to time points by unique ID rather than by an explicit
    time. This should help us if messages get crossed on the wire.

---
+++ src/Clapi/Types/Dkmap.hs
@@ -0,0 +1,86 @@
+{-# OPTIONS_GHC -Wall -Wno-orphans #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TupleSections #-}
+
+module Clapi.Types.Dkmap where
+
+-- | Doubly-keyed maps.
+
+import Prelude hiding (fail)
+-- import Control.Lens (Index, IxValue, Ixed(..), At(..), makeLenses)
+import Control.Monad (when)
+import Control.Monad.Fail (MonadFail(..))
+import Data.Bimap (Bimap)
+import qualified Data.Bimap as Bimap
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+data Dkmap k0 k1 v
+  = Dkmap {_keyMap :: (Bimap k1 k0), _valueMap :: (Map k0 v)}
+  deriving (Show, Eq, Functor, Foldable)
+-- makeLenses ''Dkmap
+
+-- type instance Index (Dkmap k0 k1 v) = k0
+-- type instance IxValue (Dkmap k0 k1 v) = v
+
+-- instance Ord k0 => Ixed (Dkmap k0 k1 v) where
+--   ix k0 = valueMap . (ix k0)
+
+-- instance Ord k0 => At (Dkmap k0 k1 v) where
+--   at k0 = valueMap . (at k0)
+
+empty :: (Ord k0, Ord k1) => Dkmap k0 k1 v
+empty = Dkmap Bimap.empty mempty
+
+-- | This is not idempotent
+insert
+  :: (MonadFail m, Ord k0, Ord k1)
+  => k0 -> k1 -> v -> Dkmap k0 k1 v -> m (Dkmap k0 k1 v)
+insert k0 k1 v (Dkmap km vm) = do
+  when (k1 `Bimap.member` km) $ fail "Secondary key already in use"
+  when (k0 `Bimap.memberR` km) $ fail "Primary key already in use"
+  return $ Dkmap (Bimap.insert k1 k0 km) (Map.insert k0 v vm)
+
+-- | This is idempotent. We only use the primary key (k0) to set the value. The
+-- secondary key (k1) must not be changed to overwrite another value.
+set
+  :: (Ord k0, Ord k1, MonadFail m)
+  => k0 -> k1 -> v -> Dkmap k0 k1 v -> m (Dkmap k0 k1 v)
+set k0 k1 v (Dkmap km vm) = do
+  case k1 `Bimap.lookup` km of
+    Nothing -> return ()
+    Just oldK0 -> when (oldK0 /= k0) $ fail "Duplicate k1"
+  return $ Dkmap (Bimap.insert k1 k0 km) (Map.insert k0 v vm)
+
+lookupK0 :: Ord k0 => k0 -> Dkmap k0 k1 v -> Maybe v
+lookupK0 k0 (Dkmap _ vm) = Map.lookup k0 vm
+
+lookupK1 :: (Ord k1, Ord k0) => k1 -> Dkmap k0 k1 v -> Maybe v
+lookupK1 k1 (Dkmap km vm) = Bimap.lookup k1 km >>= flip Map.lookup vm
+
+deleteK0 :: (Ord k0, Ord k1) => k0 -> Dkmap k0 k1 v -> Dkmap k0 k1 v
+deleteK0 k0 (Dkmap km vm) = Dkmap (Bimap.deleteR k0 km) (Map.delete k0 vm)
+
+-- | Primary key must be present in the Dkmap
+deleteK0'
+  :: (Ord k0, Ord k1, MonadFail m) => k0 -> Dkmap k0 k1 v -> m (Dkmap k0 k1 v)
+deleteK0' k0 (Dkmap km vm) = let (mOldV, vm') = Map.alterF (,Nothing) k0 vm in
+  case mOldV of
+    Nothing -> fail "Primary key not in map"
+    _ -> return $ Dkmap (Bimap.deleteR k0 km) vm'
+
+deleteK1 :: (Ord k1, Ord k0) => k1 -> Dkmap k0 k1 v -> Dkmap k0 k1 v
+deleteK1 k1 (Dkmap km vm) =
+    Dkmap (Bimap.delete k1 km)
+    (maybe vm id $ flip Map.delete vm <$> Bimap.lookup k1 km)
+
+-- | For a given `k0` we want to change `k1`
+rekey
+  :: (Ord k0, Ord k1, MonadFail m)
+  => k0 -> k1 -> Dkmap k0 k1 v -> m (Dkmap k0 k1 v)
+rekey k0 k1' (Dkmap km vm) = do
+  k1 <- maybe (fail "missing") return $ Bimap.lookupR k0 km
+  return $ Dkmap (Bimap.insert k1' k0 $ Bimap.delete k1 km) vm
