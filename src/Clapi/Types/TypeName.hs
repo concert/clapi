87365dc4bf4ded0831cdd20a5799e0c9142e319a
David Honour <david@concertdaw.co.uk>
2018-05-18 16:43:42 +0100

    Work out the text serialisation stuff that the Path implementation had

--- src/Clapi/Types/TypeName.hs
+++ src/Clapi/Types/TypeName.hs
@@ -2,29 +2,69 @@
    DataKinds
  , KindSignatures
  , GADTs
+ , OverloadedStrings
+ , ScopedTypeVariables
+ , TypeApplications
+ , LambdaCase
 #-}
 
 module Clapi.Types.TypeName where
 
-import Clapi.Types.Path (Seg)
+import Prelude hiding (fail)
+import Control.Monad.Fail (MonadFail(..))
+import qualified Data.Attoparsec.Text as DAT
+import Data.Attoparsec.Text (Parser)
+import Data.Monoid ((<>))
+import qualified Data.Text as Text
+import Data.Text (Text)
+
+import Clapi.Types.Path (Seg(unSeg), segP)
 
 data TypeNamespace = TnTree | TnValue deriving (Eq, Show, Bounded, Enum)
-
-newtype RawTypeName = RawTypeName (Seg, Seg)
 
 data TypeName :: TypeNamespace -> * where
     TreeTypeName :: Seg -> Seg -> TypeName TnTree
     ValueTypeName :: Seg -> Seg -> TypeName TnValue
 
-toRawTypeName :: TypeName a -> (RawTypeName, TypeNamespace)
-toRawTypeName (TreeTypeName ns n) = (RawTypeName (ns, n), TnTree)
-toRawTypeName (ValueTypeName ns n) = (RawTypeName (ns, n), TnValue)
+qualSepChar :: Char
+qualSepChar = ':'
 
-data AnyTypeName
-  = AtnTree (TypeName TnTree)
-  | AtnValue (TypeName TnValue)
+typeNameToText :: TypeName a -> Text
+typeNameToText tn = case tn of
+    TreeTypeName ns s -> t ns s
+    ValueTypeName ns s -> t ns s
+  where
+    t ns s = unSeg ns <> Text.singleton qualSepChar <> unSeg s
 
-fromRawTypeName :: TypeNamespace -> RawTypeName -> AnyTypeName
-fromRawTypeName tns (RawTypeName (ns, n)) = case tns of
-    TnTree -> AtnTree $ TreeTypeName ns n
-    TnValue -> AtnValue $ ValueTypeName ns n
+instance Show (TypeName a) where
+  show tn = Text.unpack (typeNameToText tn)
+
+getTypeNameP :: (Seg -> Seg -> TypeName a) -> Parser (TypeName a)
+getTypeNameP con = con <$> segP <*> (DAT.char qualSepChar >> segP)
+
+class TypeNameParseable a where
+    typeNameP :: Parser (TypeName a)
+
+instance TypeNameParseable TnTree where
+    typeNameP = getTypeNameP TreeTypeName
+
+instance TypeNameParseable TnValue where
+    typeNameP = getTypeNameP ValueTypeName
+
+typeNameFromText :: TypeNameParseable a => MonadFail m => Text -> m (TypeName a)
+typeNameFromText =
+    either fail return . DAT.parseOnly (typeNameP <* DAT.endOfInput)
+
+data ChildTypeName a
+  = CtnCont (TypeName a)
+  | CtnConc (TypeName TnValue)
+
+childTypeNameToText :: ChildTypeName a -> Text
+childTypeNameToText stn = case stn of
+    CtnCont tn -> Text.singleton 'C' <> typeNameToText tn
+    CtnConc tn -> Text.singleton 'V' <> typeNameToText tn
+
+childTypeNameP :: forall a. TypeNameParseable a => Parser (ChildTypeName a)
+childTypeNameP = DAT.anyChar >>= \case
+    'C' -> CtnCont <$> typeNameP @a
+    'V' -> CtnConc <$> typeNameP @TnValue
