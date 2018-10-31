6b390b0c0511e4d95a1049d851806e5aa7b19b68
Paul Weaver <paul@concertdaw.co.uk>
2018-10-31 14:02:34 +0000

    Moar WIP - this time moar working

--- src/Clapi/Types/Wire.hs
+++ src/Clapi/Types/Wire.hs
@@ -1,14 +1,20 @@
 {-# LANGUAGE
     AllowAmbiguousTypes
+  , ConstraintKinds
+  , DataKinds
   , ExistentialQuantification
   , FlexibleInstances
   , GADTs
+  , InstanceSigs
   , LambdaCase
   , MultiParamTypeClasses
   , PolyKinds
   , Rank2Types
   , StandaloneDeriving
   , TemplateHaskell
+  , TypeFamilies
+  , TypeFamilyDependencies
+  , TypeInType
   , TypeOperators
 #-}
 
@@ -27,141 +33,73 @@ import Prelude hiding (fail)
 
 import Control.Monad.Fail (MonadFail(..))
 import Data.Bifunctor (bimap)
+import Data.Constraint (Dict(..), mapDict, ins)
 import Data.Int
+import Data.Kind (Type, Constraint)
 import Data.Text (Text)
 import Data.Word
 import Data.Type.Equality (TestEquality(..), (:~:))
 import Data.Typeable
 import Data.Maybe (fromJust)
 
+import GHC.TypeLits (ErrorMessage((:<>:)))
+import qualified GHC.TypeLits as Lits
+
 import Clapi.Serialisation.Base (Encodable)
 import Clapi.Types.Base (Time(..))
 import Clapi.Types.WireTH (mkWithWtProxy)
 import Clapi.Util (proxyF, proxyF3)
 
--- data NewWireValue a where
---   NwvTime :: Time -> NewWireValue Time
---   NwvWord32 :: Word32 -> NewWireValue Word32
---   NwvWord64 :: Word64 -> NewWireValue Word64
---   NwvInt32 :: Int32 -> NewWireValue Int32
---   NwvInt64 :: Int64 -> NewWireValue Int64
---   NwvFloat :: Float -> NewWireValue Float
---   NwvDouble :: Double -> NewWireValue Double
---   NwvText :: Text -> NewWireValue Text
---   NwvList :: [x] -> NewWireValue [x]
---   NwvMaybe :: Maybe x -> NewWireValue (Maybe x)
---   NwvPair :: (x, y) -> NewWireValue (x, y)
 
-data SomeWireable where
-  SomeWireable :: NewWireable a => a -> SomeWireable
+-- data WireType
+--   = WtTime
+--   | WtWord32 | WtWord64
+--   | WtInt32 | WtInt64
+--   | WtFloat | WtDouble
+--   | WtString
+--   | WtList WireType
+--   | WtMaybe WireType
+--   | WtPair WireType WireType
+--   deriving (Show, Eq, Ord)
 
+data WireType a where
+  WtTime :: WireType Time
+  WtWord32 :: WireType Word32
+  WtList :: WireType x -> WireType [x]
+  WtPair :: WireType x -> WireType y -> WireType (x, y)
 
-awtRefl :: AnotherWT a :~: AnotherWT b -> a :~: b
-awtRefl Refl = Refl
+deriving instance Eq (WireType a)
+-- deriving instance Ord (WireType a)
 
-instance Eq SomeWireable where
-  SomeWireable wv1 == SomeWireable wv2 = awtyEq' wv1 wv2
-
-instance Ord SomeWireable where
-  SomeWireable wv1 `compare` SomeWireable wv2 = undefined
-
--- nwvList :: [NewWireValue x] -> NewWireValue [x]
--- nwvList = NwvList . fmap unwrapNwv
-
--- nwvMaybe :: Maybe (NewWireValue x) -> NewWireValue (Maybe x)
--- nwvMaybe = NwvMaybe . fmap unwrapNwv
-
--- nwvPair :: (NewWireValue x, NewWireValue y) -> NewWireValue (x, y)
--- nwvPair = NwvPair . bimap unwrapNwv unwrapNwv
-
-class (Eq a, Ord a) => NewWireable a where
-  toWireType :: proxy a -> WireType
-  toAnotherWireType :: AnotherWT a
-
-instance NewWireable Time where
-  toWireType _ = WtTime
-  toAnotherWireType = AwtTime
-
-instance NewWireable Word32 where
-  toWireType _ = WtWord32
-  toAnotherWireType = undefined
-
-instance NewWireable a => NewWireable [a] where
-  toWireType _ = WtList $ toWireType $ Proxy @a
-  toAnotherWireType = AwtList $ toAnotherWireType @a
-
-instance NewWireable a => NewWireable (Maybe a) where
-  toWireType _ = WtMaybe $ toWireType $ Proxy @a
-  toAnotherWireType = undefined
-
-instance (NewWireable a, NewWireable b) => NewWireable (a, b) where
-  toWireType _ = WtPair (toWireType $ Proxy @a) (toWireType $ Proxy @b)
-  toAnotherWireType = AwtPair
-    (toAnotherWireType @a)
-    (toAnotherWireType @b)
-
-
-data AnotherWT a where
-  AwtTime :: AnotherWT Time
-  AwtList :: AnotherWT a -> AnotherWT [a]
-  AwtPair :: AnotherWT x -> AnotherWT y -> AnotherWT (x, y)
-
-deriving instance Eq (AnotherWT a)
-
-data AwtConstructor = AwtTimeC | AwtListC | AwtPairC deriving (Eq, Ord)
-
-awtToC :: AnotherWT a -> AwtConstructor
-awtToC = \case
-  AwtTime -> AwtTimeC
-  AwtList _ -> AwtListC
-  AwtPair _ _ -> AwtPairC
-
-instance Ord (AnotherWT a) where
-  compare AwtTime AwtTime = EQ
-  compare (AwtList awt1) (AwtList awt2) = compare awt1 awt2
-  compare (AwtPair awt1x awt1y) (AwtPair awt2x awt2y) =
-    compare (awt1x, awt1y) (awt2x, awt2y)
-  compare awt1 awt2 = compare (awtToC awt1) (awtToC awt2)
-
-data SomeAwt where
-  SomeAwt :: AnotherWT a -> SomeAwt
-
-instance TestEquality AnotherWT where
-  testEquality AwtTime AwtTime = Just Refl
-  testEquality (AwtList awt1) (AwtList awt2) = snarf <$> testEquality awt1 awt2
-  testEquality (AwtPair awt1x awt1y) (AwtPair awt2x awt2y) =
-    reflPair <$> testEquality awt1x awt2x <*> testEquality awt1y awt2y
+instance TestEquality WireType where
+  testEquality WtTime WtTime = Just Refl
+  testEquality WtWord32 WtWord32 = Just Refl
+  testEquality (WtList wt1) (WtList wt2) = liftRefl <$> testEquality wt1 wt2
+  testEquality (WtPair wt1x wt1y) (WtPair wt2x wt2y) =
+    pairRefl <$> testEquality wt1x wt2x <*> testEquality wt1y wt2y
   testEquality _ _ = Nothing
 
+liftRefl :: a :~: b -> f a :~: f b
+liftRefl Refl = Refl
 
-awtyEq' :: forall a b. (Eq a, NewWireable a, NewWireable b) => a -> b -> Bool
-awtyEq' a b = awtyEq (toAnotherWireType @a) a (toAnotherWireType @b) b
+pairRefl :: a :~: b -> c :~: d -> (a, c) :~: (b, d)
+pairRefl Refl Refl = Refl
 
-awtyEq :: Eq a => AnotherWT a -> a -> AnotherWT b -> b -> Bool
-awtyEq awt1 a awt2 b = case testEquality awt1 awt2 of
-  Nothing -> False
-  Just Refl -> a == b
+getEq :: WireType a -> Dict (Eq a)
+getEq = \case
+  WtTime -> Dict
+  WtWord32 -> Dict
+  WtList wt -> mapDict ins $ getEq wt
+  WtPair wt1 wt2 -> pairDict (getEq wt1) (getEq wt2)
 
-awtyOrd :: Ord a => AnotherWT a -> a -> AnotherWT b -> b -> Ordering
-awtyOrd awt1 a awt2 b = case testEquality awt1 awt2 of
-  Nothing -> undefined
-  Just Refl -> a `compare` b
+pairDict :: Dict (Eq a) -> Dict (Eq b) -> Dict (Eq (a, b))
+pairDict Dict Dict = Dict
 
+data WireValue where
+  WireValue :: WireType a -> a -> WireValue
 
-snarf :: a :~: b -> f a :~: f b
-snarf Refl = Refl
-
-reflPair :: a1 :~: a2 -> b1 :~: b2 -> (a1, b1) :~: (a2, b2)
-reflPair Refl Refl = Refl
-
-
-data WireType
-  = WtTime
-  | WtWord32 | WtWord64
-  | WtInt32 | WtInt64
-  | WtFloat | WtDouble
-  | WtString
-  | WtList WireType
-  | WtMaybe WireType
-  | WtPair WireType WireType
-  deriving (Show, Eq, Ord)
+instance Eq WireValue where
+  WireValue wt1 a1 == WireValue wt2 a2 = case testEquality wt1 wt2 of
+    Nothing -> False
+    Just Refl -> case getEq wt1 of
+      Dict -> a1 == a2
