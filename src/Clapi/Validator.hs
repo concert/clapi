80f54021fb91b2db84c8e1d8dbe817158501f473
Paul Weaver <paul@concertdaw.co.uk>
2018-11-13 15:04:40 +0000

    Update Validator to use shiny new types

--- src/Clapi/Validator.hs
+++ src/Clapi/Validator.hs
@@ -11,23 +11,28 @@
 module Clapi.Validator where
 
 import Prelude hiding (fail)
-import Control.Monad (void)
+import Control.Monad (void, (>=>))
 import Control.Monad.Fail (MonadFail(..))
-import Data.Finite (packFinite)
-import Data.Word (Word32)
+import Data.Constraint (Dict(..), mapDict, ins)
+import qualified Data.Set as Set
 import Data.Tagged (Tagged(..))
 import Data.Text (Text)
 import qualified Data.Text as Text
 import Data.Type.Equality ((:~:)(..))
+import Data.Word (Word32)
 import Text.Regex.PCRE ((=~~))
 import Text.Printf (printf, PrintfArg)
 
-import Clapi.Util (safeToEnum)
-import Clapi.Types (WireValue(..), WireType(..), Definition, enumVal)
+import Clapi.Util (safeToEnum, ensureUnique)
+import Clapi.Types
+  (WireValue(..), SomeWireValue(..), WireType(..), WireTypeOf, Definition)
+import Clapi.Types.EnumVal (enumVal)
 import Clapi.Types.Path (Seg, Path)
 import qualified Clapi.Types.Path as Path
 import Clapi.Types.Tree
-  ( Bounds, boundsMin, boundsMax, TreeType(..), SomeTreeType(..))
+  ( Bounds, boundsMin, boundsMax, TreeType(..), SomeTreeType(..)
+  , TreeValue(..))
+import Clapi.Types.UniqList (mkUniqList)
 
 inBounds :: (Ord a, MonadFail m, PrintfArg a) => Bounds a -> a -> m a
 inBounds b n = go (boundsMin b) (boundsMax b)
@@ -55,9 +60,18 @@ checkEnum ns w = let theMax = fromIntegral $ length ns in
     else return w
 
 
-typeValid :: MonadFail m => WireType a1 -> TreeType a2 b -> m (a1 :~: a2)
+typeValid
+  :: MonadFail m => WireType a -> TreeType b -> m (a :~: WireTypeOf b)
 typeValid WtTime TtTime = return Refl
 typeValid WtWord32 (TtWord32 _) = return Refl
+typeValid WtWord64 (TtWord64 _) = return Refl
+typeValid WtInt32 (TtInt32 _) = return Refl
+typeValid WtInt64 (TtInt64 _) = return Refl
+typeValid WtFloat (TtFloat _) = return Refl
+typeValid WtDouble (TtDouble _) = return Refl
+typeValid WtString tt1
+  | TtString _ <- tt1 = return Refl
+  | TtRef _ <- tt1 = return Refl
 typeValid (WtList wt) tt1
   | TtList tt2 <- tt1 = (\Refl -> Refl) <$> typeValid wt tt2
   | TtSet tt2 <- tt1 = (\Refl -> Refl) <$> typeValid wt tt2
@@ -67,24 +81,55 @@ typeValid (WtPair wt1 wt2) (TtPair tt1 tt2) = do
   Refl <- typeValid wt1 tt1
   Refl <- typeValid wt2 tt2
   return $ Refl
+typeValid _ _ = fail "useful message here"
 
-validateValue :: MonadFail m => TreeType a b -> a -> m b
+validateValue :: MonadFail m => TreeType a -> WireTypeOf a -> m a
 validateValue = \case
     TtTime -> return
-    TtEnum _ -> enumVal
+    TtEnum sl -> enumVal sl
     TtWord32 b -> inBounds b
+    TtWord64 b -> inBounds b
+    TtInt32 b -> inBounds b
+    TtInt64 b -> inBounds b
+    TtFloat b -> inBounds b
+    TtDouble b -> inBounds b
     TtString pat -> checkString pat
     TtRef _ -> Path.fromText Path.segP
     TtList tt -> mapM $ validateValue tt
+    TtSet tt -> case ordyShowy tt of
+      Dict -> mapM (validateValue tt) >=> ensureUnique "items"
+              >=> return . Set.fromList
+    TtOrdSet tt -> case ordyShowy tt of
+      Dict -> mapM (validateValue tt) >=> mkUniqList
+    TtMaybe tt -> mapM $ validateValue tt
     TtPair tt1 tt2 -> \(x, y) ->
       (,) <$> validateValue tt1 x <*> validateValue tt2 y
+  where
+    ordyShowy :: TreeType a -> Dict (Ord a, Show a)
+    ordyShowy = \case
+      TtTime -> Dict
+      TtEnum _ -> Dict
+      TtWord32 _ -> Dict
+      TtWord64 _ -> Dict
+      TtInt32 _ -> Dict
+      TtInt64 _ -> Dict
+      TtFloat _ -> Dict
+      TtDouble _ -> Dict
+      TtString _ -> Dict
+      TtRef _ -> Dict
+      TtList tt -> case ordyShowy tt of Dict -> Dict
+      TtSet tt -> case ordyShowy tt of Dict -> Dict
+      TtOrdSet tt -> case ordyShowy tt of Dict -> Dict
+      TtMaybe tt -> case ordyShowy tt of Dict -> Dict
+      TtPair tt1 tt2 -> case (ordyShowy tt1, ordyShowy tt2) of
+        (Dict, Dict) -> Dict
 
-validate :: MonadFail m => TreeType a b -> WireValue -> m b
+validate :: MonadFail m => TreeType a -> WireValue b -> m (TreeValue a)
 validate tt (WireValue wt a) = do
   Refl <- typeValid wt tt
-  validateValue tt a
+  TreeValue tt <$> validateValue tt a
 
-extractTypeAssertions :: TreeType a b -> b -> [(Tagged Definition Seg, Path)]
+extractTypeAssertions :: TreeType a -> a -> [(Tagged Definition Seg, Path)]
 extractTypeAssertions = \case
   TtRef s -> \path -> [(Tagged s, path)]
   TtList tt -> foldMap (extractTypeAssertions tt)
