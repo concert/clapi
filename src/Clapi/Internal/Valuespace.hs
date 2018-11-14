7d504445037a2aabd559a86eeafdcd42d4639b7b
Paul Weaver <paul@concertdaw.co.uk>
2018-11-14 14:17:49 +0000

    Generate lenses for the components of Valuespace
    We're going to get stateful about validation and I want to use the lenses
    to update parts of the state easily. This will break a bunch of stuff, but
    we're well past the point where everything is on fire anyway!

--- src/Clapi/Internal/Valuespace.hs
+++ src/Clapi/Internal/Valuespace.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE
+    TemplateHaskell
+#-}
 module Clapi.Internal.Valuespace
-  ( Valuespace(..)
+  ( Valuespace(Valuespace)
+  , vsTree, vsPostDefs, vsTyDefs, vsTyAssns, vsXrefs, vsRootEditable
   , DefMap, TypeAssignmentMap, Referer, Referee, Xrefs
   ) where
 
+import Control.Lens (makeLenses)
 import Data.Map (Map)
 import Data.Set (Set)
 import Data.Tagged (Tagged)
@@ -14,7 +19,7 @@ import Clapi.Types.Definitions
   (Definition, PostDefinition, Editable, DefName)
 import Clapi.Types.Digests (TpId)
 import Clapi.Types.Path (Seg, Path)
-import Clapi.Types.Wire (WireValue)
+import Clapi.Types.Tree (SomeTreeValue(..))
 
 type DefMap def = Map (Tagged def Seg) def
 type TypeAssignmentMap = Dependencies Path DefName
@@ -23,10 +28,12 @@ type Referee = Path
 type Xrefs = Map Referee (Map Referer (Maybe (Set TpId)))
 
 data Valuespace = Valuespace
-  { vsTree :: RoseTree [WireValue]
-  , vsPostDefs :: DefMap PostDefinition
-  , vsTyDefs :: DefMap Definition
-  , vsTyAssns :: TypeAssignmentMap
-  , vsXrefs :: Xrefs
-  , vsRootEditable :: Editable
+  { _vsTree :: RoseTree [SomeTreeValue]
+  , _vsPostDefs :: DefMap PostDefinition
+  , _vsTyDefs :: DefMap Definition
+  , _vsTyAssns :: TypeAssignmentMap
+  , _vsXrefs :: Xrefs
+  , _vsRootEditable :: Editable
   } deriving (Show)
+
+makeLenses ''Valuespace
