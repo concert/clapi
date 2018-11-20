d0d36bf2836e592a62fe3c8c8b898d83a9aa5679
Paul Weaver <paul@concertdaw.co.uk>
2018-11-20 11:25:58 +0000

    Build a more permissive casting function
    This allows us to downcast so long as we don't change the meaning of a
    value. However, it does mean that we have to add runtime failure back in.

--- src/Clapi/Types/EnumVal.hs
+++ src/Clapi/Types/EnumVal.hs
@@ -12,11 +12,12 @@ import Prelude hiding (fail, (!!))
 import Control.Monad.Fail (MonadFail(..))
 import Data.Constraint (Dict(..))
 import Data.Word
+import Text.Printf (printf)
 
 import GHC.TypeLits (Symbol, SomeSymbol(..), symbolVal)
 
 import Clapi.Types.PNat
-  (SPNat, SomePNat(..), type (<), (%<), (:<=:))
+  (SPNat, SomePNat(..), type (<), (%<), type (<=), (:<=:))
 import qualified Clapi.Types.PNat as PNat
 import Clapi.Types.SymbolList
   (SymbolList, Length, (!!), IsPrefixOf(..), prefixProvesLte)
@@ -54,6 +55,8 @@ enumVal sl w = case PNat.fromWord32 w of
 enumVal_ :: MonadFail m => [String] -> Word32 -> m SomeEnumVal
 enumVal_ ss w = SL.withSymbolList (fmap SomeEnumVal . flip enumVal w) ss
 
+-- | Expand the type of an EnumVal to include a larger set of options, whilst
+--   guaranteeing that existing values will not change meaning.
 upcast
   :: forall ss1 ss2.
   ss1 `IsPrefixOf` ss2
@@ -67,3 +70,20 @@ upcast (EnumVal _ sPNat) sl =
       => Length ss1 :<=: Length ss2 -> SPNat n -> Dict (n < Length ss2)
     blah lteProof _ = PNat.ltDict $
       PNat.extendLt (PNat.proofLT @n @(Length ss1)) lteProof
+
+-- | Change the type of an EnumVal to a different set of options, making sure
+--   that _this_ value doesn't change it's meaning
+castValue
+  :: forall m ss1 ss2. MonadFail m
+  => EnumVal ss1 -> SymbolList ss2 -> m (EnumVal ss2)
+castValue (EnumVal sl1 n) sl2 = case n %< SL.length sl2 of
+    Nothing -> fail $ printf "Value outside range %s" (show $ SL.toStrings sl2)
+    Just proof -> case PNat.ltDict proof of
+      Dict -> let v1 = sl1 !! n; v2 = sl2 !! n in
+        if v1 == v2
+        then return $ EnumVal sl2 n
+        else fail $
+          printf "Cannot cast value '%s' to '%s'" (symString v1) (symString v2)
+  where
+    symString :: SomeSymbol -> String
+    symString (SomeSymbol p) = symbolVal p
